b9b88468dc48a04fcf00f7affc1dee13b235e40a
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2024-01-14 20:58:51 -0300

    ...

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,7 +1,21 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+for(let contador = 0; contador < listaDeTeclas.length; contador ++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    //console.log(contador);
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
+
--- desafios/desafio-01/main.js
+++ desafios/desafio-01/main.js
@@ -1,1 +1,12 @@
 const listaDeTeclas = document.querySelectorAll('input[type=button]');
+const inputTel = document.querySelector('input[type=tel]');
+
+for (let indice = 0; indice < listaDeTeclas.length; indice++) {
+  
+  const tecla = listaDeTeclas[indice];
+  console.log(tecla);
+  
+  tecla.onclick = function () {
+    inputTel.value = `${inputTel.value + tecla.value}`;
+}
+}
