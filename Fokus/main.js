2fa43debcc5ec9cdfe00b97bf4d5dc204ed4ec9a
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2024-02-18 21:13:41 -0300

    ...

--- Fokus/main.js
+++ Fokus/main.js
@@ -18,7 +18,7 @@ const audioIniciado = new Audio('sons/play.wav');
 const audioPausa = new Audio('sons/pause.mp3');
 
 
-let tempoDecorridoEmSegundos = 10;
+let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
 
 musica.loop = true;
@@ -32,7 +32,7 @@ musicaFocoInput.addEventListener('change', () => {
 });
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 10;
+    tempoDecorridoEmSegundos = 1500;
     alterarContexto('foco');
     focoBt.classList.add('active');
 });
