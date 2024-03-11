1749d4c35216dcca05f0d1d75a696fb3abee3b97
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2024-03-11 15:19:09 -0300

    Mostrando a mensagem na tela

--- numero-secreto/app/reconhecimentoDeVoz.js
+++ numero-secreto/app/reconhecimentoDeVoz.js
@@ -1,10 +1,21 @@
+const elementoChute = document.querySelector('#chute');
+
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition()
 recognition.lang = 'pt-br';
-recognition.start   ();                                                                                      
+recognition.start();
+
 recognition.addEventListener('result', onSpeak); 
 
-function onSpeak(e) {
-    console.log(e);
+function onSpeak(event) {
+    chute = event.results[0][0].transcript;
+    exibeChuteNaTela(chute);
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse:</div>
+        <span class="box">${chute}</span>
+    `
 }
