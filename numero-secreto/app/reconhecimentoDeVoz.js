c31843adf9b652492dcf532a203b5044b741070d
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2024-03-11 14:22:11 -0300

    ...

--- numero-secreto/app/reconhecimentoDeVoz.js
+++ numero-secreto/app/reconhecimentoDeVoz.js
@@ -2,4 +2,9 @@ window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition()
 recognition.lang = 'pt-br';
-recognition.start();
+recognition.start   ();                                                                                      
+recognition.addEventListener('result', onSpeak); 
+
+function onSpeak(e) {
+    console.log(e);
+}
